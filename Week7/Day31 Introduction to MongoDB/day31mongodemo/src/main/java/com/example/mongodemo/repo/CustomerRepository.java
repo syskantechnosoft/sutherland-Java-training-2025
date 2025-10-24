ddb89b49146ffb9f9d73fd4b96584bf7d8b888f9
Siva OS <syskantechnosoft@users.noreply.github.com>
2025-10-24 16:35:55 +0530

    Update CustomerRepository.java

--- Week7/Day31 Introduction to MongoDB/day31mongodemo/src/main/java/com/example/mongodemo/repo/CustomerRepository.java
+++ Week7/Day31 Introduction to MongoDB/day31mongodemo/src/main/java/com/example/mongodemo/repo/CustomerRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.mongodemo.model.Customer;
 
@@ -12,4 +13,17 @@ public interface CustomerRepository extends MongoRepository<Customer, String> {
 	List<Customer> findByFirstName(String firstName);
 	List<Customer> findByLastName (String lastName);
 	Optional<Customer> findByEmail(String email);
+	
+	List<Customer> findByCity(String city);
+	
+	@Query("{'age': {$gt:?0}}")
+	List<Customer> findCustomersOlderThan(int age);
+	
+	@Query("{ 'city': ?0, 'age': { $lt: ?1 } }")
+	List<Customer> findByCityAndYoungerThan(String city, int age);
+
+	@Query("{ 'name': { $regex: ?0, $options: 'i' } }")
+	List<Customer> findByNameLike(String namePattern);
+
+
 }
